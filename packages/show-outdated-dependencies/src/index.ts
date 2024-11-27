979b03a6695a608ce6d470a6c7e5db8ab1cc7377
RobertW123 <r.wirtz@chainsulting.de>
2024-11-27 12:43:03 +0100

    feat: Added check if version bump is valid

--- packages/show-outdated-dependencies/src/index.ts
+++ packages/show-outdated-dependencies/src/index.ts
@@ -1,49 +1,122 @@
 import dotenv from 'dotenv';
-import { exec } from 'node:child_process';
 import fs from 'node:fs';
 import path from 'node:path';
-import { OutdatedDependentPackage, Package, PackagePack, PackageView } from './types';
+import { Package, PackagePack, PackageView } from './types';
+import { execute, getOutdatedDependentPackages, isValidVersionBump } from './utils';
 
 dotenv.config();
 
-const execute = (command: string): Promise<string> => {
-	return new Promise((resolve, reject) => {
-		exec(command, (error, stdout) => {
-			if (error) {
-				reject(error);
-			}
-			resolve(stdout);
-		});
-	});
-};
-
-const getOutdatedDependentPackages = (packages: Array<Package>): Array<OutdatedDependentPackage> => {
-	const outdatedDependentPackages = new Array<OutdatedDependentPackage>();
-	for (const { packageJson: dependent } of packages) {
-		if (dependent.dependencies) {
-			const outdatedDependentPackage: OutdatedDependentPackage = {
-				dependentName: dependent.name,
-				dependencies: [],
-			};
-			for (const { packageJson: workspacePackageJson } of packages) {
-				for (const dependency of Object.entries(dependent.dependencies)) {
-					const dependencyName = dependency[0];
-					const dependencyVersion = dependency[1].replace('^', '');
-					if (workspacePackageJson.name === dependencyName && workspacePackageJson.version !== dependencyVersion) {
-						outdatedDependentPackage.dependencies.push({
-							name: workspacePackageJson.name,
-							oldVersion: dependencyVersion,
-							newVersion: workspacePackageJson.version,
-						});
-					}
-				}
-			}
-			if (outdatedDependentPackage.dependencies.length > 0) {
-				outdatedDependentPackages.push(outdatedDependentPackage);
-			}
+const showPackages = (packages: Array<Package>): void => {
+	const outputLines = new Array<string>();
+	for (const { packageJson } of packages) {
+		outputLines.push(`${packageJson.name} ${packageJson.version}` + (packageJson.private ? ' (private)' : ''));
+	}
+	if (outputLines.length === 0) {
+		console.log('No packages found.');
+		process.exit(1); // eslint-disable-line unicorn/no-process-exit
+	} else {
+		console.log('Packages:');
+		for (const line of outputLines) {
+			console.log(line);
 		}
 	}
-	return outdatedDependentPackages;
+};
+
+const showInvalidVersionBumps = (packages: Array<Package>): void => {
+	const outputLines = new Array<string>();
+	for (const { packagePack, packageView } of packages) {
+		if (
+			packagePack &&
+			packageView &&
+			packagePack.version !== packageView.version &&
+			!isValidVersionBump(packagePack, packageView)
+		) {
+			outputLines.push(`${packagePack.name} ${packageView.version} -> ${packagePack.version}`);
+		}
+	}
+	if (outputLines.length === 0) {
+		console.log('No invalid version bump.');
+	} else {
+		console.log('Invalid version bumps:');
+		for (const line of outputLines) {
+			console.log(line);
+		}
+		process.exit(1); // eslint-disable-line unicorn/no-process-exit
+	}
+};
+
+const showPackageVersionsToUpdate = (packages: Array<Package>): void => {
+	const outputLines = new Array<string>();
+	for (const { packagePack, packageView } of packages) {
+		if (
+			packagePack &&
+			packageView &&
+			packagePack.version === packageView.version &&
+			packagePack.shasum !== packageView.dist.shasum
+		) {
+			outputLines.push(`${packagePack.name} ${packagePack.version} -> xxx`);
+		}
+	}
+	if (outputLines.length === 0) {
+		console.log('No package version needs to be updated.');
+	} else {
+		console.log('Package versions that need to be updated:');
+		for (const line of outputLines) {
+			console.log(line);
+		}
+		process.exit(1); // eslint-disable-line unicorn/no-process-exit
+	}
+};
+
+const showOutdatedDependencies = (packages: Array<Package>): void => {
+	const outputLines = new Array<string>();
+	let fixOutput = '';
+	const outdatedDependencyPackages = getOutdatedDependentPackages(packages);
+	for (const { dependentName, dependencies } of outdatedDependencyPackages) {
+		outputLines.push(`${dependentName}`);
+		for (const { name: dependencyName, oldVersion, newVersion } of dependencies) {
+			outputLines.push(`\t${dependencyName} ${oldVersion} -> ${newVersion}`);
+			if (fixOutput) {
+				fixOutput += ' && ';
+			}
+			const tmpFix = `npm i ${dependencyName}@${newVersion} --workspace ${dependentName}`;
+			fixOutput += `${tmpFix} && ${tmpFix}`;
+		}
+	}
+	if (outputLines.length === 0) {
+		console.log('No outdated dependencies found.');
+	} else {
+		console.log('Outdated dependencies:');
+		for (const line of outputLines) {
+			console.log(line);
+		}
+		console.log(`\n\tFix: ${fixOutput}`);
+		process.exit(1); // eslint-disable-line unicorn/no-process-exit
+	}
+};
+
+const showPackgaesToPublish = (packages: Array<Package>): void => {
+	const outputLines = new Array<string>();
+	let fixOutput = '';
+	for (const { packagePack, packageView } of packages) {
+		if (packagePack && packageView && packagePack.version !== packageView.version) {
+			outputLines.push(`${packagePack.name} ${packageView.version} -> ${packagePack.version}`);
+			if (fixOutput) {
+				fixOutput += ' && ';
+			}
+			fixOutput += `npm publish --workspace ${packagePack.name} && git tag ${packagePack.name.split('/')[1]}-v${packagePack.version}`;
+		}
+	}
+	if (outputLines.length === 0) {
+		console.log('No package needs to be published.');
+	} else {
+		console.log('Packages that need to be published:');
+		for (const line of outputLines) {
+			console.log(line);
+		}
+		console.log(`\n\tFix: ${fixOutput} && git push --tags`);
+		process.exit(1); // eslint-disable-line unicorn/no-process-exit
+	}
 };
 
 const main = async (): Promise<void> => {
@@ -67,8 +140,8 @@ const main = async (): Promise<void> => {
 						return {
 							packagePath,
 							packageJson,
-							packageView: undefined,
 							packagePack: undefined,
+							packageView: undefined,
 						};
 					} else {
 						const packageView = JSON.parse(await execute(`npm view --json ${packageJson.name}`)) as PackageView;
@@ -78,92 +151,29 @@ const main = async (): Promise<void> => {
 						return {
 							packagePath,
 							packageJson,
-							packageView,
 							packagePack,
+							packageView,
 						};
 					}
 				}),
 			)
 		).filter(Boolean) as Array<Package>;
 
-		let exitCode = 0;
-
-		// Show package versions that need to be updated
-		let outputLines = new Array<string>();
-		for (const { packageView, packagePack } of packages) {
-			if (
-				packageView &&
-				packagePack &&
-				packageView.version === packagePack.version &&
-				packageView.dist.shasum !== packagePack.shasum
-			) {
-				outputLines.push(`${packagePack.name} ${packagePack.version} -> xxx`);
-			}
-		}
-		if (outputLines.length === 0) {
-			console.log('No package version needs to be updated');
-		} else {
-			exitCode = 1;
-			console.log('Package versions that need to be updated:');
-			for (const line of outputLines) {
-				console.log(line);
-			}
-		}
-
-		// Show outdated dependencies
-		const outdatedDependencyPackages = getOutdatedDependentPackages(packages);
-		outputLines = [];
-		let fixOutput = '';
-		for (const { dependentName, dependencies } of outdatedDependencyPackages) {
-			outputLines.push(`${dependentName}`);
-			for (const { name: dependencyName, oldVersion, newVersion } of dependencies) {
-				outputLines.push(`\t${dependencyName} ${oldVersion} -> ${newVersion}`);
-				if (fixOutput) {
-					fixOutput += ' && ';
-				}
-				const tmpFix = `npm i ${dependencyName}@${newVersion} --workspace ${dependentName}`;
-				fixOutput += `${tmpFix} && ${tmpFix}`;
-			}
-		}
-		if (outputLines.length === 0) {
-			console.log('\nNo outdated dependencies found');
-		} else {
-			exitCode = 1;
-			console.log('\nOutdated dependencies:');
-			for (const line of outputLines) {
-				console.log(line);
-			}
-			console.log(`\n\tFix: ${fixOutput}`);
-		}
-
-		// Show packages that need to be published
-		outputLines = [];
-		fixOutput = '';
-		for (const { packageView, packagePack } of packages) {
-			if (packageView && packagePack && packageView.version !== packagePack.version) {
-				outputLines.push(`${packagePack.name} ${packageView.version} -> ${packagePack.version}`);
-				if (fixOutput) {
-					fixOutput += ' && ';
-				}
-				fixOutput += `npm publish --workspace ${packagePack.name} && git tag ${packagePack.name.split('/')[1]}-v${packagePack.version}`;
-			}
-		}
-		if (outputLines.length === 0) {
-			console.log('\nNo package needs to be published');
-		} else {
-			exitCode = 1;
-			console.log('\nPackages that need to be published:');
-			for (const line of outputLines) {
-				console.log(line);
-			}
-			console.log(`\n\tFix: ${fixOutput} && git push --tags`);
-		}
-
-		if (exitCode !== 0) {
-			process.exit(exitCode); // eslint-disable-line unicorn/no-process-exit
-		}
+		showPackages(packages);
+		console.log('');
+		showInvalidVersionBumps(packages);
+		console.log('');
+		showPackageVersionsToUpdate(packages);
+		console.log('');
+		showOutdatedDependencies(packages);
+		console.log('');
+		showPackgaesToPublish(packages);
 	} catch (error) {
-		console.error(error);
+		if (error instanceof Error) {
+			console.error(error.message);
+		} else {
+			console.error(error);
+		}
 		process.exit(1); // eslint-disable-line unicorn/no-process-exit
 	}
 };
