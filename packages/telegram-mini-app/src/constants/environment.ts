754ca3211f83909be76217c2324e06c6713a5b13
Marvin Ottersberg <m.ottersberg@chainsulting.de>
2024-12-02 14:19:56 +0100

    bug: log envs

--- packages/telegram-mini-app/src/constants/environment.ts
+++ packages/telegram-mini-app/src/constants/environment.ts
@@ -2,3 +2,5 @@ export const BRIDGE_URL = process.env.REACT_APP_BRIDGE_URL ?? '';
 export const API_KEY = process.env.REACT_APP_API_KEY ?? '';
 export const APP_URL = process.env.REACT_APP_APP_URL ?? '';
 export const APP_ICON = process.env.REACT_APP_APP_ICON;
+
+console.log("ENVS: ", BRIDGE_URL, API_KEY, APP_URL, APP_ICON);
